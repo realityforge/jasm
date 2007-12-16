923076ef6fcf4c9c4962630213e2b4ee36886958
Peter Donald <peter@realityforge.org>
2007-12-16 09:50:57 +0000

    Code restructuring.

--- Assembler/src/jasm/InstructionSet.java
+++ Assembler/src/jasm/InstructionSet.java
@@ -8,47 +8,28 @@
  */
 package jasm;
 
-
-/**
- * Instruction Set Architecture monikers.
- *
- * @author Bernd Mathiske
- */
 public enum InstructionSet {
+  AMD64(Category.CISC, RelativeAddressing.FROM_INSTRUCTION_END),
+  IA32(Category.CISC, RelativeAddressing.FROM_INSTRUCTION_END),
+  PPC(Category.RISC, RelativeAddressing.FROM_INSTRUCTION_START),
+  SPARC(Category.RISC, RelativeAddressing.FROM_INSTRUCTION_START);
 
-    AMD64(Category.CISC, RelativeAddressing.FROM_INSTRUCTION_END),
-    IA32(Category.CISC, RelativeAddressing.FROM_INSTRUCTION_END),
-    PPC(Category.RISC, RelativeAddressing.FROM_INSTRUCTION_START),
-    SPARC(Category.RISC, RelativeAddressing.FROM_INSTRUCTION_START);
+  public enum Category { CISC, RISC }
+  public enum RelativeAddressing { FROM_INSTRUCTION_START, FROM_INSTRUCTION_END }
 
-    public enum Category {
-        CISC, RISC;
-    }
+  private final RelativeAddressing _relativeAddressing;
+  private final Category _category;
 
-    private final Category _category;
+  private InstructionSet(Category category, RelativeAddressing relativeAddressing) {
+    _category = category;
+    _relativeAddressing = relativeAddressing;
+  }
 
-    public Category category() {
-        return _category;
-    }
+  public Category category() { return _category; }
+  public RelativeAddressing relativeAddressing() { return _relativeAddressing; }
 
-    public enum RelativeAddressing {
-        FROM_INSTRUCTION_START, FROM_INSTRUCTION_END;
-    }
-
-    private final RelativeAddressing _relativeAddressing;
-
-    public RelativeAddressing relativeAddressing() {
-        return _relativeAddressing;
-    }
-
-    private InstructionSet(Category category, RelativeAddressing relativeAddressing) {
-        _category = category;
-        _relativeAddressing = relativeAddressing;
-    }
-
-    @Override
-    public String toString() {
-        return name();
-    }
-
+  @Override
+  public String toString() {
+    return name();
+  }
 }
