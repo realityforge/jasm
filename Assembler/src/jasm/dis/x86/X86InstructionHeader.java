7a841fc8132609204242cc357b6c21fc35a467fd
Peter Donald <peter@realityforge.org>
2007-12-16 18:42:05 +0000

    Add toString to make it easier to debug

--- Assembler/src/jasm/dis/x86/X86InstructionHeader.java
+++ Assembler/src/jasm/dis/x86/X86InstructionHeader.java
@@ -25,96 +25,109 @@ import java.util.Map;
  *
  * @author Bernd Mathiske
  */
-public class X86InstructionHeader {
+public final class X86InstructionHeader {
 
-    protected boolean _hasAddressSizePrefix;
-    protected HexByte _rexPrefix;
-    protected HexByte _instructionSelectionPrefix;
-    protected HexByte _opcode1;
-    protected HexByte _opcode2;
+  boolean _hasAddressSizePrefix;
+  HexByte _rexPrefix;
+  HexByte _instructionSelectionPrefix;
+  HexByte _opcode1;
+  HexByte _opcode2;
 
-    X86InstructionHeader() {
+  X86InstructionHeader() {
+  }
+
+  @Override
+  public String toString() {
+    return "X86InstructionHeader[AddressPrefix=" + _hasAddressSizePrefix +
+           ",rexPrefix=" + _rexPrefix +
+           ",instructionSelectionPrefix=" + _instructionSelectionPrefix +
+           ",opcode1=" + _opcode1 +
+           ",opcode2=" + _opcode2 +
+           "]";
+  }
+
+  private X86InstructionHeader(WordWidth addressWidth, X86Template template) {
+    _hasAddressSizePrefix = template.addressSizeAttribute() != addressWidth;
+    _instructionSelectionPrefix = template.instructionSelectionPrefix();
+    if (template.operandSizeAttribute() == WordWidth.BITS_16) {
+      assert _instructionSelectionPrefix == null;
+      _instructionSelectionPrefix = X86Opcode.OPERAND_SIZE;
     }
+    _opcode1 = template.opcode1();
+    _opcode2 = template.opcode2();
+  }
 
-    private X86InstructionHeader(WordWidth addressWidth, X86Template template) {
-        _hasAddressSizePrefix = template.addressSizeAttribute() != addressWidth;
-        _instructionSelectionPrefix = template.instructionSelectionPrefix();
-        if (template.operandSizeAttribute() == WordWidth.BITS_16) {
-            assert _instructionSelectionPrefix == null;
-            _instructionSelectionPrefix = X86Opcode.OPERAND_SIZE;
-        }
-        _opcode1 = template.opcode1();
-        _opcode2 = template.opcode2();
+  @Override
+  public boolean equals(Object other) {
+    if (other instanceof X86InstructionHeader) {
+      final X86InstructionHeader header = (X86InstructionHeader) other;
+      return _hasAddressSizePrefix == header._hasAddressSizePrefix &&
+             _instructionSelectionPrefix == header._instructionSelectionPrefix &&
+             _opcode1 == header._opcode1 &&
+             _opcode2 == header._opcode2;
     }
+    return false;
+  }
 
-    @Override
-    public boolean equals(Object other) {
-        if (other instanceof X86InstructionHeader) {
-            final X86InstructionHeader header = (X86InstructionHeader) other;
-            return _hasAddressSizePrefix == header._hasAddressSizePrefix &&
-                _instructionSelectionPrefix == header._instructionSelectionPrefix && _opcode1 == header._opcode1 && _opcode2 == header._opcode2;
-        }
-        return false;
+  @Override
+  public int hashCode() {
+    int result = _hasAddressSizePrefix ? -1 : 1;
+    if (_instructionSelectionPrefix != null) {
+      result *= _instructionSelectionPrefix.ordinal();
     }
-
-    @Override
-    public int hashCode() {
-        int result = _hasAddressSizePrefix ? -1 : 1;
-        if (_instructionSelectionPrefix != null) {
-            result *= _instructionSelectionPrefix.ordinal();
-        }
-        if (_opcode1 != null) {
-            result *= _opcode1.ordinal();
-        }
-        if (_opcode2 != null) {
-            result ^= _opcode2.ordinal();
-        }
-        if (_instructionSelectionPrefix != null) {
-            result += _instructionSelectionPrefix.ordinal() * 1024;
-        }
-        if (_opcode2 != null) {
-            result += _opcode2.ordinal() * 256;
-        }
-        if (_opcode1 != null) {
-            result += _opcode1.ordinal();
-        }
-        return result;
+    if (_opcode1 != null) {
+      result *= _opcode1.ordinal();
     }
+    if (_opcode2 != null) {
+      result ^= _opcode2.ordinal();
+    }
+    if (_instructionSelectionPrefix != null) {
+      result += _instructionSelectionPrefix.ordinal() * 1024;
+    }
+    if (_opcode2 != null) {
+      result += _opcode2.ordinal() * 256;
+    }
+    if (_opcode1 != null) {
+      result += _opcode1.ordinal();
+    }
+    return result;
+  }
 
-    public static <Template_Type extends X86Template> Map<X86InstructionHeader, AppendableSequence<Template_Type>> createMapping(Assembly<Template_Type> assembly, WordWidth addressWidth) {
-        final Map<X86InstructionHeader, AppendableSequence<Template_Type>> result = new HashMap<X86InstructionHeader, AppendableSequence<Template_Type>>();
-        for (Template_Type template : assembly.templates()) {
-            X86InstructionHeader header = new X86InstructionHeader(addressWidth, template);
-            AppendableSequence<Template_Type> matchingTemplates = result.get(header);
-            if (matchingTemplates == null) {
-                matchingTemplates = new ArrayListSequence<Template_Type>();
-                result.put(header, matchingTemplates);
+  public static <Template_Type extends X86Template> Map<X86InstructionHeader, AppendableSequence<Template_Type>> createMapping(
+      Assembly<Template_Type> assembly, WordWidth addressWidth) {
+    final Map<X86InstructionHeader, AppendableSequence<Template_Type>> result =
+        new HashMap<X86InstructionHeader, AppendableSequence<Template_Type>>();
+    for (Template_Type template : assembly.templates()) {
+      X86InstructionHeader header = new X86InstructionHeader(addressWidth, template);
+      AppendableSequence<Template_Type> matchingTemplates = result.get(header);
+      if (matchingTemplates == null) {
+        matchingTemplates = new ArrayListSequence<Template_Type>();
+        result.put(header, matchingTemplates);
+      }
+      matchingTemplates.append(template);
+      for (X86Parameter parameter : template.parameters()) {
+        switch (parameter.place()) {
+          case OPCODE1_REXB:
+          case OPCODE1:
+            for (int i = 0; i < 8; i++) {
+              header = new X86InstructionHeader(addressWidth, template);
+              header._opcode1 = HexByte.values()[header._opcode1.ordinal() + i];
+              result.put(header, matchingTemplates);
             }
-            matchingTemplates.append(template);
-            for (X86Parameter parameter : template.parameters()) {
-                switch (parameter.place()) {
-                    case OPCODE1_REXB:
-                    case OPCODE1:
-                        for (int i = 0; i < 8; i++) {
-                            header = new X86InstructionHeader(addressWidth, template);
-                            header._opcode1 = HexByte.values()[header._opcode1.ordinal() + i];
-                            result.put(header, matchingTemplates);
-                        }
-                        break;
-                    case OPCODE2_REXB:
-                    case OPCODE2:
-                        for (int i = 0; i < 8; i++) {
-                            header = new X86InstructionHeader(addressWidth, template);
-                            header._opcode2 = HexByte.values()[header._opcode2.ordinal() + i];
-                            result.put(header, matchingTemplates);
-                        }
-                        break;
-                    default:
-                        break;
-                }
+            break;
+          case OPCODE2_REXB:
+          case OPCODE2:
+            for (int i = 0; i < 8; i++) {
+              header = new X86InstructionHeader(addressWidth, template);
+              header._opcode2 = HexByte.values()[header._opcode2.ordinal() + i];
+              result.put(header, matchingTemplates);
             }
+            break;
+          default:
+            break;
         }
-        return result;
+      }
     }
-
+    return result;
+  }
 }
