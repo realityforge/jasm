858a1d96b1fed15b7ec9152a91dae5730e307893
Peter Donald <peter@realityforge.org>
2007-12-21 13:31:33 +0000

    Extract out a few more classes

--- Assembler/src/jasm/gen/test/AssemblyTester.java
+++ Assembler/src/jasm/gen/test/AssemblyTester.java
@@ -22,11 +22,8 @@ import jasm.util.SimpleTimer;
 import jasm.util.WordWidth;
 import jasm.util.collect.AppendableSequence;
 import jasm.util.collect.ArrayListSequence;
-import jasm.util.collect.ArraySequence;
-import jasm.util.collect.FilterIterator;
 import jasm.util.collect.Sequence;
 import jasm.util.io.IndentWriter;
-import jasm.util.lang.StaticLoophole;
 import jasm.util.program.ProgramError;
 import jasm.util.program.ProgramWarning;
 import jasm.util.program.Trace;
@@ -47,7 +44,6 @@ import java.util.EnumSet;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map.Entry;
-import java.util.NoSuchElementException;
 import java.util.Set;
 
 /**
@@ -133,181 +129,7 @@ public abstract class AssemblyTester<Template_Type extends Template, Disassemble
       }
   }
 
-  enum TestCaseLegality {
-            LEGAL, ILLEGAL_BY_CONSTRAINT, ILLEGAL_BY_ARGUMENT
-    }
-
-    /**
-     * This is an iterator over a (lazily generated) selection of test cases for a given template.
-     * Using an iterator means that each test case is generated as needed and does not
-     * use up memory longer than necessary. This helps prevent out of memory errors
-     * for templates where the number of test cases can be very large.
-     *
-     * It is important to note that the value returned by {@link #next()} is only valid
-     * until {@code next()} is called again. That is, the same <code>Sequence<Argument></code>
-     * object is returned by each call to {@code next()}, only its contents have changed.
-     *
-     * @author Doug Simon
-     */
-    class ArgumentListIterator implements Iterator<Sequence<Argument>> {
-
-        private final Template_Type _template;
-        private final Parameter[] _parameters;
-        private final Iterator<? extends Argument>[] _testArgumentIterators;
-        private final int _count;
-        private final Argument[] _arguments;
-        private final Sequence<Argument> _next;
-        private final TestCaseLegality _testCaseLegality;
-
-        private boolean _hasNext;
-        private boolean _advanced;
-        private int _iterations;
-
-        /**
-         * Creates an iterator over a set of test cases for a given template.
-         */
-        ArgumentListIterator(Template_Type template, TestCaseLegality testCaseLegality) {
-            _testCaseLegality  = testCaseLegality;
-            _template = template;
-            _parameters = Sequence.Static.toArray(template.parameters(), Parameter.class);
-            _count = template.parameters().length();
-            _arguments = new Argument[_count];
-            _next = new ArraySequence<Argument>(_arguments);
-            _testArgumentIterators = StaticLoophole.cast(new Iterator[_count]);
-            _hasNext = advance();
-        }
-
-        /**
-         * @return the number of times {@link #next} has been invoked on this iterator without throwing a NoSuchElementException.
-         */
-        public int iterations() {
-            return _iterations;
-        }
-
-        public boolean hasNext() {
-            if (_count == 0) {
-                return _testCaseLegality == TestCaseLegality.LEGAL && _iterations == 0;
-            }
-            if (!_advanced) {
-                _hasNext = advance();
-            }
-            return _hasNext;
-        }
-
-        /**
-         * The returned sequence is only valid for a single iteration and so should be copied
-         * if needed after this iteration.
-         */
-        public Sequence<Argument> next() {
-            if (!hasNext()) {
-                throw new NoSuchElementException();
-            }
-            _advanced = false;
-            ++_iterations;
-            return _next;
-        }
-
-        /**
-         * Advances this iterator to the next argument list.
-         *
-         * @return true if the iterator could advance
-         */
-        private boolean advance() {
-            assert !_advanced;
-            _advanced = true;
-            if (_count == 0) {
-                return _testCaseLegality == TestCaseLegality.LEGAL;
-            }
-            boolean result;
-            do {
-                result = advance0();
-            } while (result && isLegalArgumentList(_template, _next) != (_testCaseLegality == TestCaseLegality.LEGAL));
-            return result;
-        }
-
-        /**
-         * Advances the test argument iterator for parameter {@code i}.
-         *
-         * @return true if the iterator could advance
-         */
-        private boolean advanceArgumentFor(int i) {
-            if (_testArgumentIterators[i].hasNext()) {
-                _arguments[i] = _testArgumentIterators[i].next();
-                return true;
-            }
-            return false;
-        }
-
-        /**
-         * Advances this iterator to the next (potentially invalid) argument list.
-         *
-         * @return true if the iterator could advance
-         */
-        private boolean advance0() {
-            for (int i = _count - 1; i >= 0; --i) {
-                if (_testArgumentIterators[i] != null) {
-                    if (advanceArgumentFor(i)) {
-                        return true;
-                    }
-                    if (i == 0) {
-                        return false;
-                    }
-                }
-
-                // Reset iterator over test arguments of the i'th parameter
-                final Parameter parameter = _parameters[i];
-                if (_testCaseLegality != TestCaseLegality.ILLEGAL_BY_ARGUMENT) {
-                    final Iterable<? extends Argument> arguments = parameter.getLegalTestArguments();
-                    final ArgumentRange argumentRange = parameter.argumentRange();
-                    if (argumentRange == null || !argumentRange.appliesInternally()) {
-                        _testArgumentIterators[i] = arguments.iterator();
-                    } else {
-                        _testArgumentIterators[i] = new FilterIterator<Argument>(arguments.iterator(), new FilterIterator.Predicate<Argument>() {
-                            public boolean evaluate(Argument argument) {
-                                return argumentRange.includes(argument);
-                            }
-                        });
-                    }
-                } else {
-                    Iterator<? extends Argument> iterator = parameter.getIllegalTestArguments().iterator();
-                    if (!iterator.hasNext()) {
-                        // For parameters that have no illegal values (e.g. a symbolic register parameter)
-                        // at least one argument must be returned otherwise illegal test cases where only
-                        // one argument is illegal will never be returned by this iterator.
-                        //
-                        // Instead of iterating over all the legal arguments for such a parameter, only
-                        // the last legal value is used. This involves retrieving the iterator twice but
-                        // that's much cheaper than many more redundant iterations.
-                        iterator = parameter.getLegalTestArguments().iterator();
-                      int count = 0;
-                      while (iterator.hasNext()) {
-                          ++count;
-                          ((Iterator) iterator).next();
-                      }
-                      int n = count;
-                        iterator = parameter.getLegalTestArguments().iterator();
-                        while (n-- > 1) {
-                            iterator.next();
-                        }
-                        assert iterator.hasNext();
-                        _testArgumentIterators[i] = iterator;
-                    } else {
-                        _testArgumentIterators[i] = iterator;
-                    }
-                }
-                if (!advanceArgumentFor(i)) {
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        public void remove() {
-            throw new UnsupportedOperationException();
-        }
-    }
-
-    /**
+  /**
      * Determines if a given set of arguments for a given template is legal.
      */
     protected abstract boolean isLegalArgumentList(Template_Type template, Sequence<Argument> arguments);
@@ -519,7 +341,7 @@ public abstract class AssemblyTester<Template_Type extends Template, Disassemble
 
     private void createExternalSource(Template_Type template, IndentWriter stream) throws IOException, InterruptedException, AssemblyException {
         if (template.isExternallyTestable()) {
-            final ArgumentListIterator argumentLists = new ArgumentListIterator(template, TestCaseLegality.LEGAL);
+            final ArgumentListIterator<Template_Type> argumentLists = new ArgumentListIterator<Template_Type>(this, template, TestCaseLegality.LEGAL);
             createExternalSource(template, argumentLists, stream);
             Trace.line(2, "template: " + template + "  [" + argumentLists.iterations() + " test cases]");
         }
@@ -572,7 +394,8 @@ public abstract class AssemblyTester<Template_Type extends Template, Disassemble
         final boolean testingExternally = _components.contains(AssemblyTestComponent.EXTERNAL_ASSEMBLER) && template.isExternallyTestable();
 
         // Process legal test cases
-        final ArgumentListIterator argumentLists = new ArgumentListIterator(template, TestCaseLegality.LEGAL);
+        final ArgumentListIterator<Template_Type> argumentLists =
+            new ArgumentListIterator<Template_Type>(this, template, TestCaseLegality.LEGAL);
         ProgramError.check(argumentLists.hasNext(), "no test cases were generated for template: " + template);
         _timer.reset();
         File binaryFile = null;
@@ -599,7 +422,8 @@ public abstract class AssemblyTester<Template_Type extends Template, Disassemble
         }
 
         int testCaseNumber = 0;
-        for (final ArgumentListIterator iterator = new ArgumentListIterator(template, TestCaseLegality.LEGAL); iterator.hasNext();) {
+        for (final ArgumentListIterator<Template_Type> iterator =
+            new ArgumentListIterator<Template_Type>(this, template, TestCaseLegality.LEGAL); iterator.hasNext();) {
             final Sequence<Argument> argumentList = iterator.next();
             final Assembler assembler = createTestAssembler();
 
@@ -649,7 +473,7 @@ public abstract class AssemblyTester<Template_Type extends Template, Disassemble
         int illegalTestCaseNumber = 0;
         final Set<String> uniqueExceptionMessages = new HashSet<String>();
         for (TestCaseLegality testCaseLegality : new TestCaseLegality[]{TestCaseLegality.ILLEGAL_BY_CONSTRAINT, TestCaseLegality.ILLEGAL_BY_ARGUMENT}) {
-            for (final ArgumentListIterator iterator = new ArgumentListIterator(template, testCaseLegality); iterator.hasNext();) {
+            for (final ArgumentListIterator<Template_Type> iterator = new ArgumentListIterator<Template_Type>(this, template, testCaseLegality); iterator.hasNext();) {
                 final Sequence<Argument> argumentList = iterator.next();
                 final Assembler assembler = createTestAssembler();
                 try {
