c3535fdbca22d754c0a8ba68b742aba0f9fa4b45
Peter Donald <peter@realityforge.org>
2007-12-22 06:18:28 +0000

    Remove some unused methods

--- src/main/java/jasm/util/Ints.java
+++ src/main/java/jasm/util/Ints.java
@@ -16,10 +16,6 @@ package jasm.util;
  */
 public final class Ints {
 
-    // Utility classes should not be instantiated.
-    private Ints() {
-    }
-
   public static int numberOfEffectiveSignedBits(int signed) {
         if (signed >= 0) {
             return 33 - Integer.numberOfLeadingZeros(signed);
@@ -30,49 +26,4 @@ public final class Ints {
     public static int numberOfEffectiveUnsignedBits(int unsigned) {
         return 32 - Integer.numberOfLeadingZeros(unsigned);
     }
-
-  public static int roundedUp(int value, int by) {
-        final int rest = value % by;
-        if (rest == 0) {
-            return value;
-        }
-        if (value < 0) {
-            return value - rest;
-        }
-        return value + (by - rest);
-    }
-
-    public static String toHexLiteral(int value) {
-        return "0x" + String.format("%08X", value);
-    }
-
-  public static boolean contains(int[] array, int value) {
-        for (int element : array) {
-            if (element == value) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public static int[] append(int[] array, int element) {
-        final int resultLength = array.length + 1;
-        final int[] result = new int[resultLength];
-        System.arraycopy(array, 0, result, 0, array.length);
-        result[array.length] = element;
-        return result;
-    }
-
-  public static int[] createRange(int first, int last) {
-        if (first > last) {
-            throw new IllegalArgumentException();
-        }
-        final int n = last + 1 - first;
-        final int[] result = new int[n];
-        for (int i = 0; i < n; i++) {
-            result[i] = first + i;
-        }
-        return result;
-    }
-
 }
