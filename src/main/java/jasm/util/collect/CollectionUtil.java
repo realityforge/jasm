ec633f48b23b8ffaf0edbfd7deea4227a19d8e8b
Peter Donald <peter@realityforge.org>
2007-12-23 02:24:02 +0000

    Optimize imports

--- src/main/java/jasm/util/collect/CollectionUtil.java
+++ src/main/java/jasm/util/collect/CollectionUtil.java
@@ -6,8 +6,6 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.Collection;
-import java.util.Collections;
 
 public final class CollectionUtil {
 
