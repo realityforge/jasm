f0f0ecba4c249a557523d40af26f84d8d0b124d0
Peter Donald <peter@realityforge.org>
2007-12-25 05:50:53 +0000

    Add license headers

--- src/main/java/jasm/gen/risc/sparc/SPARC32PerformanceTester.java
+++ src/main/java/jasm/gen/risc/sparc/SPARC32PerformanceTester.java
@@ -1,3 +1,11 @@
+/*
+ *  This file is part of the jasm project (http://code.google.com/p/jasm).
+ *
+ *  This file is licensed to you under the BSD License; You may not use
+ *  this file except in compliance with the License. See the LICENSE.txt
+ *  file distributed with this work for a copy of the License and information
+ *  regarding copyright ownership.
+ */
 package jasm.gen.risc.sparc;
 
 import jasm.dis.Disassembler;
