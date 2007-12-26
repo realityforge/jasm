cef979b042fb565c223a105729a4ad72f394a0e3
Peter Donald <peter@realityforge.org>
2007-12-26 15:41:42 +0000

    SImplify creation of the opcodes

--- src/main/java/jasm/tools/cisc/ia32/TwoByteOpcodeMap.java
+++ src/main/java/jasm/tools/cisc/ia32/TwoByteOpcodeMap.java
@@ -37,6 +37,7 @@ import static jasm.tools.cisc.x86.OperandCode.Qd;
 import static jasm.tools.cisc.x86.OperandCode.Qq;
 import static jasm.tools.cisc.x86.OperandCode.Rd;
 import jasm.tools.cisc.x86.X86InstructionDescriptionCreator;
+import jasm.tools.cisc.x86.X86InstructionDescription;
 import static jasm.util.HexByte._00;
 import static jasm.util.HexByte._01;
 import static jasm.util.HexByte._02;
@@ -187,197 +188,209 @@ import static jasm.util.HexByte._FA;
 import static jasm.util.HexByte._FC;
 import static jasm.util.HexByte._FD;
 import static jasm.util.HexByte._FE;
+import jasm.util.ArrayUtil;
 import static jasm.x86.SegmentRegister.FS;
 import static jasm.x86.SegmentRegister.GS;
+import java.util.ArrayList;
 
 final class TwoByteOpcodeMap extends X86InstructionDescriptionCreator {
 
   private void create_low() {
-    define(_0F, _00, GROUP_6a);
-    define(_0F, _00, GROUP_6b);
-    define(_0F, _01, GROUP_7a);
-    define(_0F, _01, GROUP_7b);
-    define(_0F, _02, "LAR", Gv, Ev); // bug in table, wrongly suggesting Ew
-    define(_0F, _03, "LSL", Gv, Ev); // bug in table, wrongly suggesting Ew
-    define(_0F, _06, "CLTS");
+    defineTwoByte(_00, GROUP_6a);
+    defineTwoByte(_00, GROUP_6b);
+    defineTwoByte(_01, GROUP_7a);
+    defineTwoByte(_01, GROUP_7b);
+    defineTwoByte(_02, "LAR", Gv, Ev); // bug in table, wrongly suggesting Ew
+    defineTwoByte(_03, "LSL", Gv, Ev); // bug in table, wrongly suggesting Ew
+    defineTwoByte(_06, "CLTS");
 
-    define(_0F, _20, "MOV", Rd, Cd);
-    define(_0F, _21, "MOV", Rd, Dd);
-    define(_0F, _22, "MOV", Cd, Rd);
-    define(_0F, _23, "MOV", Dd, Rd);
+    defineTwoByte(_20, "MOV", Rd, Cd);
+    defineTwoByte(_21, "MOV", Rd, Dd);
+    defineTwoByte(_22, "MOV", Cd, Rd);
+    defineTwoByte(_23, "MOV", Dd, Rd);
 
-    define(_0F, _30, "WRMSR");
-    define(_0F, _31, "RDTSC");
-    define(_0F, _32, "RDMSR");
-    define(_0F, _33, "RDPMC");
+    defineTwoByte(_30, "WRMSR");
+    defineTwoByte(_31, "RDTSC");
+    defineTwoByte(_32, "RDMSR");
+    defineTwoByte(_33, "RDPMC");
 
-    define(_0F, _40, "CMOVO", Gv, Ev);
-    define(_0F, _41, "CMOVNO", Gv, Ev);
-    define(_0F, _42, "CMOVB", Gv, Ev);
-    define(_0F, _43, "CMOVNB", Gv, Ev);
-    define(_0F, _44, "CMOVZ", Gv, Ev);
-    define(_0F, _45, "CMOVNZ", Gv, Ev);
-    define(_0F, _46, "CMOVBE", Gv, Ev);
-    define(_0F, _47, "CMOVNBE", Gv, Ev);
+    defineTwoByte(_40, "CMOVO", Gv, Ev);
+    defineTwoByte(_41, "CMOVNO", Gv, Ev);
+    defineTwoByte(_42, "CMOVB", Gv, Ev);
+    defineTwoByte(_43, "CMOVNB", Gv, Ev);
+    defineTwoByte(_44, "CMOVZ", Gv, Ev);
+    defineTwoByte(_45, "CMOVNZ", Gv, Ev);
+    defineTwoByte(_46, "CMOVBE", Gv, Ev);
+    defineTwoByte(_47, "CMOVNBE", Gv, Ev);
 
-    define(_0F, _60, "PUNPCKLBW", Pq, Qd);
-    define(_0F, _61, "PUNPCKLWD", Pq, Qd);
-    define(_0F, _62, "PUNPCKLDQ", Pq, Qd);
-    define(_0F, _63, "PACKSSWB", Pq, Qd);
-    define(_0F, _64, "PCMPGTB", Pq, Qd);
-    define(_0F, _65, "PCMPGTW", Pq, Qd);
-    define(_0F, _66, "PCMPGTD", Pq, Qd);
-    define(_0F, _67, "PACKUSWB", Pq, Qd);
+    defineTwoByte(_60, "PUNPCKLBW", Pq, Qd);
+    defineTwoByte(_61, "PUNPCKLWD", Pq, Qd);
+    defineTwoByte(_62, "PUNPCKLDQ", Pq, Qd);
+    defineTwoByte(_63, "PACKSSWB", Pq, Qd);
+    defineTwoByte(_64, "PCMPGTB", Pq, Qd);
+    defineTwoByte(_65, "PCMPGTW", Pq, Qd);
+    defineTwoByte(_66, "PCMPGTD", Pq, Qd);
+    defineTwoByte(_67, "PACKUSWB", Pq, Qd);
 
-    define(_0F, _70, "PSHUFW", Pq, Qd, Ib);
-    define(_0F, _71, GROUP_11);
-    define(_0F, _72, GROUP_12);
-    define(_0F, _73, GROUP_13);
-    define(_0F, _74, "PCMPEQB", Pq, Qd);
-    define(_0F, _75, "PCMPEQW", Pq, Qd);
-    define(_0F, _76, "PCMPEQD", Pq, Qd);
-    define(_0F, _77, "EMMS");
+    defineTwoByte(_70, "PSHUFW", Pq, Qd, Ib);
+    defineTwoByte(_71, GROUP_11);
+    defineTwoByte(_72, GROUP_12);
+    defineTwoByte(_73, GROUP_13);
+    defineTwoByte(_74, "PCMPEQB", Pq, Qd);
+    defineTwoByte(_75, "PCMPEQW", Pq, Qd);
+    defineTwoByte(_76, "PCMPEQD", Pq, Qd);
+    defineTwoByte(_77, "EMMS");
 
-    define(_0F, _80, "JO", Jv);
-    define(_0F, _81, "JNO", Jv);
-    define(_0F, _82, "JB", Jv);
-    define(_0F, _83, "JNB", Jv);
-    define(_0F, _84, "JZ", Jv);
-    define(_0F, _85, "JNZ", Jv);
-    define(_0F, _86, "JBE", Jv);
-    define(_0F, _87, "JNBE", Jv);
+    defineTwoByte(_80, "JO", Jv);
+    defineTwoByte(_81, "JNO", Jv);
+    defineTwoByte(_82, "JB", Jv);
+    defineTwoByte(_83, "JNB", Jv);
+    defineTwoByte(_84, "JZ", Jv);
+    defineTwoByte(_85, "JNZ", Jv);
+    defineTwoByte(_86, "JBE", Jv);
+    defineTwoByte(_87, "JNBE", Jv);
 
-    define(_0F, _90, "SETO", Eb);
-    define(_0F, _91, "SETNO", Eb);
-    define(_0F, _92, "SETB", Eb);
-    define(_0F, _93, "SETNB", Eb);
-    define(_0F, _94, "SETZ", Eb);
-    define(_0F, _95, "SETNZ", Eb);
-    define(_0F, _96, "SETBE", Eb);
-    define(_0F, _97, "SETNBE", Eb);
+    defineTwoByte(_90, "SETO", Eb);
+    defineTwoByte(_91, "SETNO", Eb);
+    defineTwoByte(_92, "SETB", Eb);
+    defineTwoByte(_93, "SETNB", Eb);
+    defineTwoByte(_94, "SETZ", Eb);
+    defineTwoByte(_95, "SETNZ", Eb);
+    defineTwoByte(_96, "SETBE", Eb);
+    defineTwoByte(_97, "SETNBE", Eb);
 
-    define(_0F, _A0, "PUSH", FS);
-    define(_0F, _A1, "POP", FS);
-    define(_0F, _A2, "CPUID");
-    define(_0F, _A3, "BT", Ev, Gv);
-    define(_0F, _A4, "SHLD", Ev, Gv, Ib);
-    define(_0F, _A5, "SHLD", Ev, Gv, CL);
-    //define(_0F, _A6, "CMPXCHG", Xb, Ts); // invalid opcode or undocumented instruction?
-    //define(_0F, _A7, "CMPXCHG", Ib, Ts); // invalid opcode or undocumented instruction?
+    defineTwoByte(_A0, "PUSH", FS);
+    defineTwoByte(_A1, "POP", FS);
+    defineTwoByte(_A2, "CPUID");
+    defineTwoByte(_A3, "BT", Ev, Gv);
+    defineTwoByte(_A4, "SHLD", Ev, Gv, Ib);
+    defineTwoByte(_A5, "SHLD", Ev, Gv, CL);
+    //defineTwoByte(_A6, "CMPXCHG", Xb, Ts); // invalid opcode or undocumented instruction?
+    //defineTwoByte(_A7, "CMPXCHG", Ib, Ts); // invalid opcode or undocumented instruction?
 
-    define(_0F, _B0, "CMPXCHG", Eb, Gb);
-    define(_0F, _B1, "CMPXCHG", Ev, Gv);
-    define(_0F, _B2, "LSS", Gv, Mp); // bug in table: no Gv
-    define(_0F, _B3, "BTR", Ev, Gv);
-    define(_0F, _B4, "LFS", Gv, Mp); // bug in table: no Gv
-    define(_0F, _B5, "LGS", Gv, Mp); // bug in table: no Gv
-    define(_0F, _B6, "MOVZXB", Gv, Eb).setExternalName("movzx");
-    define(_0F, _B7, "MOVZXW", Gv, Ew).beNotExternallyTestable(); // gas unnecessarily prepends the operand size prefix 0x66
+    defineTwoByte(_B0, "CMPXCHG", Eb, Gb);
+    defineTwoByte(_B1, "CMPXCHG", Ev, Gv);
+    defineTwoByte(_B2, "LSS", Gv, Mp); // bug in table: no Gv
+    defineTwoByte(_B3, "BTR", Ev, Gv);
+    defineTwoByte(_B4, "LFS", Gv, Mp); // bug in table: no Gv
+    defineTwoByte(_B5, "LGS", Gv, Mp); // bug in table: no Gv
+    defineTwoByte(_B6, "MOVZXB", Gv, Eb).setExternalName("movzx");
+    defineTwoByte(_B7, "MOVZXW", Gv, Ew).beNotExternallyTestable(); // gas unnecessarily prepends the operand size prefix 0x66
 
-    define(_0F, _C0, "XADD", Eb, Gb);
-    define(_0F, _C1, "XADD", Ev, Gv);
-    define(_0F, _C7, GROUP_9);
+    defineTwoByte(_C0, "XADD", Eb, Gb);
+    defineTwoByte(_C1, "XADD", Ev, Gv);
+    defineTwoByte(_C7, GROUP_9);
 
-    define(_0F, _D1, "PSRLW", Pq, Qd);
-    define(_0F, _D2, "PSRLD", Pq, Qd);
-    define(_0F, _D3, "PSRLQ", Pq, Qd);
-    define(_0F, _D5, "PMULLW", Pq, Qd);
+    defineTwoByte(_D1, "PSRLW", Pq, Qd);
+    defineTwoByte(_D2, "PSRLD", Pq, Qd);
+    defineTwoByte(_D3, "PSRLQ", Pq, Qd);
+    defineTwoByte(_D5, "PMULLW", Pq, Qd);
 
-    define(_0F, _E1, "PSRAW", Pq, Qd);
-    define(_0F, _E2, "PSRAD", Pq, Qd);
-    define(_0F, _E5, "PMULHW", Pq, Qd);
+    defineTwoByte(_E1, "PSRAW", Pq, Qd);
+    defineTwoByte(_E2, "PSRAD", Pq, Qd);
+    defineTwoByte(_E5, "PMULHW", Pq, Qd);
 
-    define(_0F, _F1, "PSLLW", Pq, Qd);
-    define(_0F, _F2, "PSLLD", Pq, Qd);
-    define(_0F, _F3, "PSLLQ", Pq, Qd);
-    define(_0F, _F5, "PMADDWD", Pq, Qd);
+    defineTwoByte(_F1, "PSLLW", Pq, Qd);
+    defineTwoByte(_F2, "PSLLD", Pq, Qd);
+    defineTwoByte(_F3, "PSLLQ", Pq, Qd);
+    defineTwoByte(_F5, "PMADDWD", Pq, Qd);
   }
 
   private void create_high() {
-    define(_0F, _08, "INVD");
-    define(_0F, _09, "WBINVD");
-    define(_0F, _0B, "UD2");
+    defineTwoByte(_08, "INVD");
+    defineTwoByte(_09, "WBINVD");
+    defineTwoByte(_0B, "UD2");
 
-    define(_0F, _48, "CMOVS", Gv, Ev);
-    define(_0F, _49, "CMOVNS", Gv, Ev);
-    define(_0F, _4A, "CMOVP", Gv, Ev);
-    define(_0F, _4B, "CMOVNP", Gv, Ev);
-    define(_0F, _4C, "CMOVL", Gv, Ev);
-    define(_0F, _4D, "CMOVNL", Gv, Ev);
-    define(_0F, _4E, "CMOVLE", Gv, Ev);
-    define(_0F, _4F, "CMOVNLE", Gv, Ev);
+    defineTwoByte(_48, "CMOVS", Gv, Ev);
+    defineTwoByte(_49, "CMOVNS", Gv, Ev);
+    defineTwoByte(_4A, "CMOVP", Gv, Ev);
+    defineTwoByte(_4B, "CMOVNP", Gv, Ev);
+    defineTwoByte(_4C, "CMOVL", Gv, Ev);
+    defineTwoByte(_4D, "CMOVNL", Gv, Ev);
+    defineTwoByte(_4E, "CMOVLE", Gv, Ev);
+    defineTwoByte(_4F, "CMOVNLE", Gv, Ev);
 
-    define(_0F, _68, "PUNPCKHBW", Pq, Qd);
-    define(_0F, _69, "PUNPCKHWD", Pq, Qd);
-    define(_0F, _6A, "PUNPCKHDQ", Pq, Qd);
-    define(_0F, _6B, "PACKSSDW", Pq, Qd);
-    define(_0F, _6E, "MOVD", Pd, Ed);
-    define(_0F, _6F, "MOVQ", Pq, Qq);
+    defineTwoByte(_68, "PUNPCKHBW", Pq, Qd);
+    defineTwoByte(_69, "PUNPCKHWD", Pq, Qd);
+    defineTwoByte(_6A, "PUNPCKHDQ", Pq, Qd);
+    defineTwoByte(_6B, "PACKSSDW", Pq, Qd);
+    defineTwoByte(_6E, "MOVD", Pd, Ed);
+    defineTwoByte(_6F, "MOVQ", Pq, Qq);
 
-    define(_0F, _7E, "MOVD", Ed, Pd);
-    define(_0F, _7F, "MOVQ", Qq, Pq);
+    defineTwoByte(_7E, "MOVD", Ed, Pd);
+    defineTwoByte(_7F, "MOVQ", Qq, Pq);
 
-    define(_0F, _88, "JS", Jv);
-    define(_0F, _89, "JNS", Jv);
-    define(_0F, _8A, "JP", Jv);
-    define(_0F, _8B, "JNP", Jv);
-    define(_0F, _8C, "JL", Jv);
-    define(_0F, _8D, "JNL", Jv);
-    define(_0F, _8E, "JLE", Jv);
-    define(_0F, _8F, "JNLE", Jv);
+    defineTwoByte(_88, "JS", Jv);
+    defineTwoByte(_89, "JNS", Jv);
+    defineTwoByte(_8A, "JP", Jv);
+    defineTwoByte(_8B, "JNP", Jv);
+    defineTwoByte(_8C, "JL", Jv);
+    defineTwoByte(_8D, "JNL", Jv);
+    defineTwoByte(_8E, "JLE", Jv);
+    defineTwoByte(_8F, "JNLE", Jv);
 
-    define(_0F, _98, "SETS", Eb);
-    define(_0F, _99, "SETNS", Eb);
-    define(_0F, _9A, "SETP", Eb);
-    define(_0F, _9B, "SETNP", Eb);
-    define(_0F, _9C, "SETL", Eb);
-    define(_0F, _9D, "SETNL", Eb);
-    define(_0F, _9E, "SETLE", Eb);
-    define(_0F, _9F, "SETNLE", Eb);
+    defineTwoByte(_98, "SETS", Eb);
+    defineTwoByte(_99, "SETNS", Eb);
+    defineTwoByte(_9A, "SETP", Eb);
+    defineTwoByte(_9B, "SETNP", Eb);
+    defineTwoByte(_9C, "SETL", Eb);
+    defineTwoByte(_9D, "SETNL", Eb);
+    defineTwoByte(_9E, "SETLE", Eb);
+    defineTwoByte(_9F, "SETNLE", Eb);
 
-    define(_0F, _A8, "PUSH", GS);
-    define(_0F, _A9, "POP", GS);
-    define(_0F, _AA, "RSM");
-    define(_0F, _AB, "BTS", Ev, Gv);
-    define(_0F, _AC, "SHRD", Ev, Gv, Ib);
-    define(_0F, _AD, "SHRD", Ev, Gv, CL);
-    define(_0F, _AF, "IMUL", Gv, Ev);
+    defineTwoByte(_A8, "PUSH", GS);
+    defineTwoByte(_A9, "POP", GS);
+    defineTwoByte(_AA, "RSM");
+    defineTwoByte(_AB, "BTS", Ev, Gv);
+    defineTwoByte(_AC, "SHRD", Ev, Gv, Ib);
+    defineTwoByte(_AD, "SHRD", Ev, Gv, CL);
+    defineTwoByte(_AF, "IMUL", Gv, Ev);
 
-    define(_0F, _B9, GROUP_10);
-    define(_0F, _BA, GROUP_8, Ev, Ib);
-    define(_0F, _BB, "BTC", Ev, Gv);
-    define(_0F, _BC, "BSF", Gv, Ev);
-    define(_0F, _BD, "BSR", Gv, Ev);
-    define(_0F, _BE, "MOVSXB", Gv, Eb).setExternalName("movsx");
-    define(_0F, _BF, "MOVSXW", Gv, Ew).beNotExternallyTestable(); // gas unnecessarily prepends the operand size prefix 0x66
+    defineTwoByte(_B9, GROUP_10);
+    defineTwoByte(_BA, GROUP_8, Ev, Ib);
+    defineTwoByte(_BB, "BTC", Ev, Gv);
+    defineTwoByte(_BC, "BSF", Gv, Ev);
+    defineTwoByte(_BD, "BSR", Gv, Ev);
+    defineTwoByte(_BE, "MOVSXB", Gv, Eb).setExternalName("movsx");
+    defineTwoByte(_BF, "MOVSXW", Gv, Ew).beNotExternallyTestable(); // gas unnecessarily prepends the operand size prefix 0x66
 
-    define(_0F, _C8, "BSWAP", God);
+    defineTwoByte(_C8, "BSWAP", God);
 
-    define(_0F, _D8, "PSUBUSB", Pq, Qq);
-    define(_0F, _D9, "PSUBUSW", Pq, Qq);
-    define(_0F, _DB, "PAND", Pq, Qq);
-    define(_0F, _DC, "PADDUSB", Pq, Qq);
-    define(_0F, _DD, "PADDUSW", Pq, Qq);
-    define(_0F, _DF, "PANDN", Pq, Qq);
+    defineTwoByte(_D8, "PSUBUSB", Pq, Qq);
+    defineTwoByte(_D9, "PSUBUSW", Pq, Qq);
+    defineTwoByte(_DB, "PAND", Pq, Qq);
+    defineTwoByte(_DC, "PADDUSB", Pq, Qq);
+    defineTwoByte(_DD, "PADDUSW", Pq, Qq);
+    defineTwoByte(_DF, "PANDN", Pq, Qq);
 
-    define(_0F, _E8, "PSUBSB", Pq, Qq);
-    define(_0F, _E9, "PSUBSW", Pq, Qq);
-    define(_0F, _EB, "POR", Pq, Qq);
-    define(_0F, _EC, "PADDSB", Pq, Qq);
-    define(_0F, _ED, "PADDSW", Pq, Qq);
-    define(_0F, _EF, "PXOR", Pq, Qq);
+    defineTwoByte(_E8, "PSUBSB", Pq, Qq);
+    defineTwoByte(_E9, "PSUBSW", Pq, Qq);
+    defineTwoByte(_EB, "POR", Pq, Qq);
+    defineTwoByte(_EC, "PADDSB", Pq, Qq);
+    defineTwoByte(_ED, "PADDSW", Pq, Qq);
+    defineTwoByte(_EF, "PXOR", Pq, Qq);
 
-    define(_0F, _F8, "PSUBB", Pq, Qq);
-    define(_0F, _F9, "PSUBW", Pq, Qq);
-    define(_0F, _FA, "PSUBD", Pq, Qq);
-    define(_0F, _FC, "PADDB", Pq, Qq);
-    define(_0F, _FD, "PADDW", Pq, Qq);
-    define(_0F, _FE, "PADDD", Pq, Qq);
+    defineTwoByte(_F8, "PSUBB", Pq, Qq);
+    defineTwoByte(_F9, "PSUBW", Pq, Qq);
+    defineTwoByte(_FA, "PSUBD", Pq, Qq);
+    defineTwoByte(_FC, "PADDB", Pq, Qq);
+    defineTwoByte(_FD, "PADDW", Pq, Qq);
+    defineTwoByte(_FE, "PADDD", Pq, Qq);
   }
 
   TwoByteOpcodeMap() {
     super(IA32Assembly.ASSEMBLY);
     create_low();
     create_high();
+  }
+
+  private X86InstructionDescription defineTwoByte(Object... specifications) {
+    final Object[] objects = ArrayUtil.flatten(specifications);
+    final ArrayList<Object> specs = new ArrayList<Object>(objects.length + 1);
+    specs.add(_0F);
+    for (Object object : objects) {
+      specs.add(object);
+    }
+    return defineInstructionDescription(specs);
   }
 }
