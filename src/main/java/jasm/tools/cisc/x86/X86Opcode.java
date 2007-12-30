40cea959ebd943fa7f7963fa92b2e886fa28487e
Peter Donald <peter@realityforge.org>
2007-12-30 09:06:35 +0000

    Add some more utility functions used when decoding instructions

--- src/main/java/jasm/tools/cisc/x86/X86Opcode.java
+++ src/main/java/jasm/tools/cisc/x86/X86Opcode.java
@@ -10,14 +10,34 @@ package jasm.tools.cisc.x86;
 
 import jasm.util.HexByte;
 import static jasm.util.HexByte.*;
-import java.util.EnumSet;
 
-/** x86 instruction prefix bytes. */
+/** Helper to determine how to decode instructions. */
 public final class X86Opcode {
-  private static final EnumSet<HexByte> FP_ESCAPE =
-      EnumSet.of(_D8, _D9, _DA, _DB, _DC, _DD, _DE, _DF);
+  public static final HexByte OPCODE1_ESCAPE = _0F;
+  public static final HexByte FP_ESCAPE_MIN = _D8;
+  public static final HexByte FP_ESCAPE_MAX = _DF;
+  public static final HexByte OPCODE2_ESCAPE1 = _38;
+  public static final HexByte OPCODE2_ESCAPE2 = _3A;
+
+  public static final HexByte FP_WAIT = _9B;
+
+  public static boolean isStandardOpcode2Prefix(final HexByte opcode) {
+    return opcode == OPCODE1_ESCAPE;
+  }
 
   public static boolean isFloatingPointEscape(HexByte opcode) {
-    return FP_ESCAPE.contains(opcode);
+    return opcode.value() >= FP_ESCAPE_MIN.value() &&
+           opcode.value() <= FP_ESCAPE_MAX.value();
+  }
+
+  //Only used in experimental decoder atm
+  public static boolean hasOpCode3(final HexByte opcode1,
+                                   final HexByte opcode2) {
+    return
+        OPCODE1_ESCAPE.byteValue() == opcode1.byteValue() &&
+        (
+            OPCODE2_ESCAPE1.byteValue() == opcode2.byteValue() ||
+            OPCODE2_ESCAPE2.byteValue() == opcode2.byteValue()
+        );
   }
 }
